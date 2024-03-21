e376d232e5c5ac635fc1dbef594128b4e4b0950e
sakibmohammad79 <sakibmohammad7679@gmail.com>
2024-03-21 19:42:51 +0600

    handle not found error

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
-
 import router from "./app/routes";
 import httpStatus from "http-status";
 import { globalErrorHandler } from "./app/middleWars/globalErorHandler";
@@ -22,5 +21,16 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use(globalErrorHandler);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "API not found!",
+    error: {
+      path: req.originalUrl,
+      message: "Your requested path not found!",
+    },
+  });
+});
 
 export default app;
