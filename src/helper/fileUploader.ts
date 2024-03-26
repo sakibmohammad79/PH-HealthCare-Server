7d8d5a132489bbcb7160002c424a79dac1d04a23
sakibmohammad79 <sakibmohammad7679@gmail.com>
2024-03-26 06:03:41 +0600

    file delete after generated image link

--- src/helper/fileUploader.ts
+++ src/helper/fileUploader.ts
@@ -1,5 +1,6 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
 
 import { v2 as cloudinary } from "cloudinary";
 cloudinary.config({
@@ -24,6 +25,7 @@ const uploadToCloudinary = async (file: any) => {
       file.path,
       { public_id: file.originalname },
       (error, result) => {
+        fs.unlinkSync(file.path);
         if (error) {
           reject(error);
         } else {
